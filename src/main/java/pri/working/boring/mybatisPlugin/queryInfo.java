af6d310bd164521628454feb33aa6ac291569058
17350 <bohan.xie@hand-china.com>
2020-09-25 16:06:56 +0800

    【ADD】增加一个mybatis拦截器练习

---
+++ src/main/java/pri/working/boring/mybatisPlugin/queryInfo.java
@@ -0,0 +1,158 @@
+package pri.working.boring.mybatisPlugin;
+
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.SqlSource;
+import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.reflection.DefaultReflectorFactory;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.reflection.factory.DefaultObjectFactory;
+import org.apache.ibatis.reflection.wrapper.DefaultObjectWrapperFactory;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author xbh
+ * @version 1.0.0
+ * @ClassName queryInfo.java
+ * @Description myabtis拦截器
+ * @createTime 2020年09月25日 11:43:00
+ */
+@Component
+@Intercepts(@Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class}))
+public class queryInfo implements Interceptor {
+
+    //定义可重入锁
+    private ReentrantLock lock = new ReentrantLock();
+
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        //分页信息
+        final Object[] args = invocation.getArgs();
+        MappedStatement ms = (MappedStatement) args[0];
+        Object parameterObject = args[1];
+        RowBounds rowBounds = (RowBounds) args[2];
+
+        //去掉mybatis自带的分页
+        args[2] = RowBounds.DEFAULT;
+
+        //BoundSql 在 Mybatis 中的作用是保存Sql
+        BoundSql boundSql = ms.getBoundSql(parameterObject);
+
+        String sql = boundSql.getSql();
+
+        String newSql = sql + " limit 10";
+
+        resetSql2Invocation(invocation, newSql);
+
+        return invocation.proceed();
+    }
+
+    @Override
+    public Object plugin(Object target) {
+        /**
+         * 只拦截执行器
+         */
+        if (target instanceof Executor) {
+            return Plugin.wrap(target, this);
+        } else {
+            return target;
+        }
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+
+    }
+
+
+    /**
+     * 获取url
+     *
+     * @param dataSource
+     * @return
+     */
+    public String getUrl(DataSource dataSource) {
+        Connection conn = null;
+        try {
+            conn = dataSource.getConnection();
+            return conn.getMetaData().getURL();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    //ignore
+                }
+            }
+        }
+    }
+
+    /**
+     * 包装sql后，重置到invocation中
+     * @param invocation
+     * @param sql
+     * @throws SQLException
+     */
+    private void resetSql2Invocation(Invocation invocation, String sql) throws SQLException {
+        final Object[] args = invocation.getArgs();
+        MappedStatement statement = (MappedStatement) args[0];
+        Object parameterObject = args[1];
+        BoundSql boundSql = statement.getBoundSql(parameterObject);
+        MappedStatement newStatement = newMappedStatement(statement, new BoundSqlSqlSource(boundSql));
+        MetaObject msObject =  MetaObject.forObject(newStatement, new DefaultObjectFactory(), new DefaultObjectWrapperFactory(),new DefaultReflectorFactory());
+        msObject.setValue("sqlSource.boundSql.sql", sql);
+        args[0] = newStatement;
+    }
+
+    private MappedStatement newMappedStatement(MappedStatement ms, SqlSource newSqlSource) {
+        MappedStatement.Builder builder =
+                new MappedStatement.Builder(ms.getConfiguration(), ms.getId(), newSqlSource, ms.getSqlCommandType());
+        builder.resource(ms.getResource());
+        builder.fetchSize(ms.getFetchSize());
+        builder.statementType(ms.getStatementType());
+        builder.keyGenerator(ms.getKeyGenerator());
+        if (ms.getKeyProperties() != null && ms.getKeyProperties().length != 0) {
+            StringBuilder keyProperties = new StringBuilder();
+            for (String keyProperty : ms.getKeyProperties()) {
+                keyProperties.append(keyProperty).append(",");
+            }
+            keyProperties.delete(keyProperties.length() - 1, keyProperties.length());
+            builder.keyProperty(keyProperties.toString());
+        }
+        builder.timeout(ms.getTimeout());
+        builder.parameterMap(ms.getParameterMap());
+        builder.resultMaps(ms.getResultMaps());
+        builder.resultSetType(ms.getResultSetType());
+        builder.cache(ms.getCache());
+        builder.flushCacheRequired(ms.isFlushCacheRequired());
+        builder.useCache(ms.isUseCache());
+
+        return builder.build();
+    }
+
+    // 定义一个内部辅助类，作用是包装sq
+    class BoundSqlSqlSource implements SqlSource {
+        private BoundSql boundSql;
+        public BoundSqlSqlSource(BoundSql boundSql) {
+            this.boundSql = boundSql;
+        }
+        @Override
+        public BoundSql getBoundSql(Object parameterObject) {
+            return boundSql;
+        }
+    }
+
+
+
+}
